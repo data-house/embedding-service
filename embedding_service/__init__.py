bc09ad6f1e697cc0789e622b5692a90e726db0d9
Matteo Mistri <matteo.mistri1996@gmail.com>
2023-06-28 12:10:33 +0200

    Add variable for strict mode

--- embedding_service/__init__.py
+++ embedding_service/__init__.py
@@ -8,6 +8,8 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.model = SentenceTransformer(
         os.environ.get("MODEL_NAME", "T-Systems-onsite/cross-en-de-roberta-sentence-transformer"))
+    app.tokenizer = app.model.tokenizer
+    app.strict_mode = os.environ.get("STRICT_MODE", "false").lower() == "true"
 
     @app.route("/embed", methods=["GET"])
     def endpoint1():
@@ -15,6 +17,11 @@ def create_app():
 
     @app.route("/embed", methods=["POST"])
     def endpoint2():
+        if not request.json or "corpus" not in request.json:
+            return {"error": "Missing corpus in json body"}, 422
+        if app.strict_mode:
+            if len(app.tokenizer.tokenize(request.json["corpus"])) > app.model.max_seq_length:
+                return {"error": "Corpus too long"}, 422
         corpus = request.json["corpus"].lower()
         return {"embedding": app.model.encode(corpus).tolist()}
 
