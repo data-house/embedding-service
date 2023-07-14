0a318f61833c17b8ea3b52a5442e3a59a5f3dd27
Alessio Vertemati <alessio.vertemati@gmail.com>
2023-07-14 11:32:48 +0200

    Change default workers to 2

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn "embedding_service:create_app()" -w "${WORKERS:-1}" --timeout "${WORKERS_TIMEOUT:-600}" -b 0.0.0.0:5000
+gunicorn "embedding_service:create_app()" -w "${WORKERS:-2}" --timeout "${WORKERS_TIMEOUT:-600}" -b 0.0.0.0:5000
